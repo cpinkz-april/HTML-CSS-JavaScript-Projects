1c3edf28e426e5ef0d731634dd46b7bfac3a1791
cpinkz-april <cp.aethaya@gmail.com>
2025-07-04 19:42:11 +0700

    Create index.js

---
+++ Multiple Questions, One at a Time/index.js
@@ -0,0 +1,38 @@
+function getAnswerA() {
+    let answer = document.getElementById("question");
+    answer.textContent = "Correct";
+}
+function getAnswerB() {
+    let answer = document.getElementById("question");
+    answer.textContent = "Incorrect";
+}
+function getAnswerC() {
+    let answer = document.getElementById("question");
+    answer.textContent = "Incorrect";
+}
+
+function getAnswer2A() {
+    let answer = document.getElementById("question2");
+    answer.textContent = "Incorrect";
+}
+function getAnswer2B() {
+    let answer = document.getElementById("question2");
+    answer.textContent = "Incorrect";
+}
+function getAnswer2C() {
+    let answer = document.getElementById("question2");
+    answer.textContent = "Correct";
+}
+
+function getAnswer3A() {
+    let answer = document.getElementById("question3");
+    answer.textContent = "Incorrect";
+}
+function getAnswer3B() {
+    let answer = document.getElementById("question3");
+    answer.textContent = "Incorrect";
+}
+function getAnswer3C() {
+    let answer = document.getElementById("question3");
+    answer.textContent = "Correct";
+}
